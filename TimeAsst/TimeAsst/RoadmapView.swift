5bb39d6800094bffc3e12a9a84e161807f00c526
Lia316 <kjk0825@postech.ac.kr>
2022-05-01 23:01:48 +0900

    feat✨ : initial commit of roadmap view (#1)

---
+++ TimeAsst/TimeAsst/RoadmapView.swift
@@ -0,0 +1,21 @@
+//
+//  RoadmapView.swift
+//  TimeAsst
+//
+//  Created by 리아 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct RoadmapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoadmapView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoadmapView()
+            .previewInterfaceOrientation(.landscapeRight)
+    }
+}
