9a18e03496069fc83e2923ddd0281ab2aa8d9462
Lia316 <kjk0825@postech.ac.kr>
2022-05-01 22:43:34 +0900

    feat✨ : set tabview (#1)

--- TimeAsst/TimeAsst/ContentView.swift
+++ TimeAsst/TimeAsst/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("달력 뷰")
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("달력")
+                }
+            Text("투두 뷰")
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                        .environment(\.symbolVariants, .none)
+                    Text("할 일")
+                }
+            Text("로드맵 뷰")
+                .tabItem {
+                    Image(systemName: "signpost.right")
+                        .environment(\.symbolVariants, .none)
+                    Text("로드맵")
+                }
+            Text("메모 뷰")
+                .tabItem {
+                    Image(systemName: "square.and.pencil")
+                    Text("메모")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
