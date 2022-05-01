498b9a81ed0f05de7a65840c31a7372aef7562b2
Lia316 <kjk0825@postech.ac.kr>
2022-05-01 21:15:29 +0900

    initial commit

---
+++ TimeAsst/TimeAsst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeAsst
+//
+//  Created by 리아 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeAsst/TimeAsst/TimeAsstApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeAsstApp.swift
+//  TimeAsst
+//
+//  Created by 리아 on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct TimeAsstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
